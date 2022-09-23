1ccd667f299d3d8bb599151fe6a1809abfcf5584
Carlos Alexandre Fernandes Batista <carlos.batista@foton.la>
2022-09-23 12:50:32 -0300

    :tada: Estrutura inicial do projeto

---
+++ src/test/java/br/com/ifg/contolefinanca/ControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifg.contolefinanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
