0cddbf5425ec70ffe8060ec35c236628cd933543
Carlos Alexandre Fernandes Batista <carlos.batista@foton.la>
2022-09-30 00:55:20 -0300

    :coffin: Removendo código morto.

--- src/main/java/br/com/ifg/controlefinanca/models/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/ifg/controlefinanca/models/config/security/WebSecurityConfig.java
@@ -21,22 +21,18 @@ import java.util.Arrays;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private Environment env;
-
     @Autowired
     UsuarioService userDetailsService;
-
+    @Autowired
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private static final String[] PUBLIC_MATCHERS = {"/h2-console/**", "/api/v*/registration/**"};
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         if(Arrays.asList(env.getActiveProfiles()).contains("DEV")){
             http.headers().frameOptions().disable();
-
         }
-
 
         http
                 .csrf().disable()
@@ -61,104 +57,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
-
-
-
-    //    //TODO: Finalizar desenvolvimento de configuração da segurança da aplicação.
-//    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
-//
-//    /** Criptografia de senha */
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//
-//    /**
-//     * Autenticação
-//     * */
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService()).passwordEncoder(bCryptPasswordEncoder());
-//
-//        /**Em Memória*/
-////        auth.inMemoryAuthentication()
-////                .withUser("user1").password(bCryptPasswordEncoder().encode("user1Pass")).roles("USER")
-////                .and()
-////                .withUser("user2").password(bCryptPasswordEncoder().encode("user2Pass")).roles("USER")
-////                .and()
-////                .withUser("admin").password(bCryptPasswordEncoder().encode("adminPass")).roles("ADMIN");
-//
-//    }
-//
-//    /**
-//     * Autorização
-//     * */
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        if(Arrays.asList(env.getActiveProfiles()).contains("DEV")){
-//            http.headers().frameOptions().disable();
-//        }
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers(PUBLIC_MATCHERS).permitAll().
-//                and()
-//                .httpBasic();
-//    }
-
-
-
-
-    /**
-     * ----------------------------------------------------------------------------------------
-     * */
-
-
-
-//
-//    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**", "/**"};
-//
-//    @Autowired
-//    private Environment env;
-//
-//    /**
-//     * Filtro EndPoint
-//     * */
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        if(Arrays.asList(env.getActiveProfiles()).contains("DEV")){
-//            http.headers().frameOptions().disable();
-//
-//        }
-//        http.cors().and().csrf().disable();
-//        http.authorizeRequests()
-//                        .antMatchers(PUBLIC_MATCHERS).permitAll()
-//                        .anyRequest().authenticated();
-//
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-//    }
-//
-////    @Override
-////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-////
-////        auth
-////                .inMemoryAuthentication()
-////                .withUser("admin")
-////                .password(encoder.encode("admin"))
-////                .roles("ADMIN", "USER")
-////                .and()
-////                .withUser("user")
-////                .password(encoder.encode("password"))
-////                .roles("USER");
-////    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource(){
-//        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 }
